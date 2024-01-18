d9affcc0ac8ee714237ff64e47f3227260f527ab
martingega <martin.gega@studenti.unimi.it>
2024-01-18 12:59:06 +0100

    Initial commit

---
+++ src/main/java/it/unimi/di/sweng/lab02/BowlingGame.java
@@ -0,0 +1,14 @@
+package it.unimi.di.sweng.lab02;
+
+public class BowlingGame {
+
+	public void roll(int pins){
+		// TODO
+	}
+
+	public int score(){
+		return -1;
+		// TODO
+	}
+
+}
---
+++ src/test/java/it/unimi/di/sweng/lab02/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package it.unimi.di.sweng.lab02;
+
+import org.junit.jupiter.api.Test;
+
+
+public class BowlingGameTest {
+
+	@Test
+	void test(){
+
+	}
+
+}
