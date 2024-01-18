e086eb43c993515bad9048c8f188dabe5c76f354
martingega <martin.gega@studenti.unimi.it>
2024-01-18 13:11:44 +0100

    implemented worst game, zero pins knocked down

--- src/test/java/it/unimi/di/sweng/lab02/BowlingGameTest.java
+++ src/test/java/it/unimi/di/sweng/lab02/BowlingGameTest.java
@@ -2,12 +2,20 @@ package it.unimi.di.sweng.lab02;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class BowlingGameTest {
 
 	@Test
-	void test(){
+	void worstGame() {
 
+		BowlingGame game = new BowlingGame();
+		for (int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
+		int score = game.score();
+		assertThat(score).isEqualTo(0);
 	}
 
 }
