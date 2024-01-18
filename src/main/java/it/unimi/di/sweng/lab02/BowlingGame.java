9d1c20964d21dbbf5a74e0d7fd1de4b50493377c
martingega <martin.gega@studenti.unimi.it>
2024-01-18 13:34:37 +0100

    implemented exception when pins number bigger than 10

--- src/main/java/it/unimi/di/sweng/lab02/BowlingGame.java
+++ src/main/java/it/unimi/di/sweng/lab02/BowlingGame.java
@@ -5,6 +5,9 @@ public class BowlingGame {
 	private int count; // count of rolled down pins
 
 	public void roll(int pins){
+		if(pins > 10) {
+			throw new IllegalArgumentException();
+		}
 		count += pins;
 	}
 
--- src/test/java/it/unimi/di/sweng/lab02/BowlingGameTest.java
+++ src/test/java/it/unimi/di/sweng/lab02/BowlingGameTest.java
@@ -3,6 +3,7 @@ package it.unimi.di.sweng.lab02;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class BowlingGameTest {
@@ -28,6 +29,13 @@ public class BowlingGameTest {
 		}
 		int score = game.score();
 		assertThat(score).isEqualTo(1);
+	}
+
+	@Test
+	void tooLargeRoll(){
+		BowlingGame game = new BowlingGame();
+		assertThatThrownBy(() -> game.roll(11))
+				.isInstanceOf(IllegalArgumentException.class);
 	}
 
 }
